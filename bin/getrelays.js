ac426f50c16f448a523378fdfaa8a20748117019
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-12-20 10:09:16 +0100

    getrelays.js script and relays.json

---
+++ bin/getrelays.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const relaysUri = "https://nostr.watch/relays.json"
+
+const https = require('https');
+
+function getJSON(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let body = '';
+      res.on('data', (d) => body += d);
+      res.on('end', () => resolve(JSON.parse(body)));
+      res.on('error', (e) => reject(e));
+    });
+  });
+}
+
+getJSON(relaysUri)
+  .then((data) => console.log(data));
+
